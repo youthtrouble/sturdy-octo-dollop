6da9d741bf675c14f2c2450e5d20249e08189a24
Deji Ajibola <harjhibolar@gmail.com>
2024-04-11 11:52:37 +0100

    feat : implement preliminary button methods

--- Assessment_2/Form1.cs
+++ Assessment_2/Form1.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +8,7 @@ namespace Assessment_2
 {
     public partial class Form1 : Form
     {
+        private CustomerTree _customerTree = new CustomerTree();
         public Form1()
         {
             InitializeComponent();
@@ -20,6 +17,32 @@ namespace Assessment_2
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+        
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            string customerName = txtName.Text;
+            int customerAge = int.Parse(txtAge.Text);
+            string customerAddress = txtAddress.Text;
+            float customerAmountOwed = float.Parse(txtAmountOwed.Text);
+            
+            Customer customer = new Customer(customerName, customerAge, customerAddress, customerAmountOwed);
+            _customerTree.InsertCustomer(customer);
+            
+            txtInsert.Clear();
+            UpdateTraversalList();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string searchName = txtSearch.Text;
+            Customer customer = _customerTree.SearchCustomer(searchName);
+        }
+        
+        private void UpdateTraversalList()
+        {
+            lstTraversal.Items.Clear();
+            _customerTree.GetCustomersPreOrder().ToList().ForEach(customer => lstTraversal.Items.Add(customer.GetInformation());
         }
     }
 }
